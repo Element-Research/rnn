9f33b4dc1557aee7e75255c30cf28e5bc951c50c
Nicholas Leonard <nick@nikopia.org>
2015-08-11 16:14:22 -0400

    refactored sensor (is backpropagatable)

--- RecurrentVisualAttention.lua
+++ RecurrentVisualAttention.lua
@@ -5,15 +5,15 @@
 -- module which takes an RNN as argument with other 
 -- hyper-parameters such as the maximum number of steps, 
 -- locator (location sampling module like ReinforceNormal) and 
--- main (classification, segmentation, etc) module.
+-- sensor (produces glimpses given image and location) module.
 ------------------------------------------------------------------------
 local RVA, parent = torch.class("nn.RecurrentVisualAttention", "nn.Container")
 
-function RVA:__init(rnn, main, locator, nStep, hiddenSize)
+function RVA:__init(rnn, sensor, locator, nStep, hiddenSize)
    parent.__init(self)
    require 'image'
    assert(torch.isTypeOf(rnn, 'nn.Module') and rnn.forget)
-   assert(torch.isTypeOf(main, 'nn.Module'))
+   assert(torch.isTypeOf(sensor, 'nn.Module'))
    assert(torch.isTypeOf(locator, 'nn.Module'))
    assert(torch.type(nStep) == 'number')
    assert(torch.type(hiddenSize) == 'table')
@@ -21,108 +21,21 @@ function RVA:__init(rnn, main, locator, nStep, hiddenSize)
    
    self.rnn = rnn
    self.rnn.copyInputs = true
-   self.main = main -- main task (classification, segmentation, etc.)
+   self.sensor = sensor -- produces glimpse given image and location
    self.locator = locator -- samples an x,y location for each example
    self.hiddenSize = hiddenSize
    self.nStep = nStep
    
-   self.modules = {self.rnn, self.main, self.locator}
+   self.modules = {self.rnn, self.sensor, self.locator}
    -- we combine these into a Container to simplify self:getSetModule()
-   local outputModule = nn.Container():add(self.main):add(self.locator)
+   local outputModule = nn.Container():add(self.sensor):add(self.locator)
    self.sharedClones = {outputModule} -- action clones
    
-   self.output = {}
-   self.glimpse = {}
-   self.hidden = {}
+   self.output = {} -- rnn output
+   self.glimpse = {} -- sensor output (called glimpses in original paper)
+   self.location = {} -- locator output
+   
    self.gradHidden = {}
-   self.location = {}
-end
-
-function RVA:initGlimpseSensor(glimpseSize, glimpseDepth, glimpseScale)
-   self.glimpseSize = glimpseSize -- height == width
-   self.glimpseDepth = glimpseDepth or 3
-   self.glimpseScale = glimplseScale or 2
-   
-   assert(torch.type(self.glimpseSize) == 'number')
-   assert(torch.type(self.glimpseDepth) == 'number')
-   assert(torch.type(self.glimpseScale) == 'number')
-end
-
--- a bandwidth limited sensor which focuses on a location.
--- locations index the x,y coord of the center of the glimpse
-function RVA:glimpseSensor(glimpse, input, location)
-   assert(self.glimpseSize, "glimpseSensor not initialize")
-   glimpse:resize(input:size(1), self.glimpseDepth, input:size(2), self.glimpseSize, self.glimpseSize)
-   
-   -- handle cuda inputs
-   local cuglimpse
-   if torch.type(glimpse) == 'torch.CudaTensor' then
-      cuglimpse = glimpse
-      self._glimpse = self._glimpse or torch.FloatTensor()
-      self._glimpse:resize(cuglimpse:size()):copy(cuglimpse)
-      glimpse = self._glimpse
-   end
-   
-   if torch.type(input) == 'torch.CudaTensor' then
-      self._input = self._input or torch.FloatTensor()
-      self._input:resize(input:size()):copy(input)
-      input = self._input
-   end
-   
-   local culocation
-   if torch.type(location) == 'torch.CudaTensor' then
-      self._location = self._location or torch.FloatTensor()
-      self._location:resize(location:size()):copy(location)
-      location = self._location
-   end
-   
-   self._crop = self._crop or glimpse.new()
-   self._pad = self._pad or input.new()
-   
-   for sampleIdx=1,glimpse:size(1) do
-      local glimpseSample = glimpse[sampleIdx]
-      local inputSample = input[sampleIdx]
-      local xy = location[sampleIdx]
-      -- (-1,-1) top left corner, (1,1) bottom right corner of image
-      local x, y = xy:select(1,1), xy:select(1,2)
-      -- (0,0), (1,1)
-      x, y = (x+1)/2, (y+1)/2
-      -- (0,0), (input:size(3)-1, input:size(4)-1)
-      x, y = x*(input:size(3)-1), y*(input:size(4)-1)
-      x = math.min(input:size(3)-1,math.max(0,x))
-      y = math.min(input:size(4)-1,math.max(0,y))
-      if x < 2 or y < 2 or x > 25 or y > 25 then
-         print("xy : "..x..", "..y)
-      end
-      
-      -- for each depth of glimpse : pad, crop, downscale
-      for depth=1,self.glimpseDepth do 
-         local dst = glimpseSample[depth]
-         local glimpseSize = self.glimpseSize*(self.glimpseScale^(depth-1))
-         
-         -- add zero padding (glimpse could be partially out of bounds)
-         local padSize = glimpseSize/2
-         self._pad:resize(input:size(2), input:size(3)+padSize*2, input:size(4)+padSize*2):zero()
-         local center = self._pad:narrow(2,padSize,input:size(3)):narrow(3,padSize,input:size(4))
-         center:copy(inputSample)
-         
-         -- coord of top-left corner of patch that will be cropped w.r.t padded input
-         -- is coord of center of patch w.r.t original non-padded input.
-        
-         -- crop it
-         self._crop:resize(input:size(2), glimpseSize, glimpseSize)
-         image.crop(self._crop, self._pad, x, y) -- crop is zero-indexed
-         image.scale(dst, self._crop)
-      end
-   end
-   
-   if cuglimpse then
-      cuglimpse:copy(glimpse)
-      glimpse = cuglimpse
-   end
-   
-   glimpse:resize(input:size(1), self.glimpseDepth*input:size(2), self.glimpseSize, self.glimpseSize)
-   return glimpse
 end
 
 function RVA:getStepModule(step)
@@ -142,8 +55,8 @@ function RVA:updateOutput(input)
    assert(nDim == 4, "only works with batch of images")
    
    for step=1,self.nStep do
-      -- we maintain a copy of main and locator (with shared params) for each time-step
-      local main, locator = self:getStepModule(step)
+      -- we maintain a copy of sensor and locator (with shared params) for each time-step
+      local sensor, locator = self:getStepModule(step)
       
       if step == 1 then
          -- sample an initial starting location by forwarding zeros through the locator
@@ -156,15 +69,11 @@ function RVA:updateOutput(input)
       end
       local location = self.location[step]
       
-      -- glimpse is the concatenation of down-scaled cropped images of increasing scale around a location
-      self.glimpse[step] = self:glimpseSensor(self.glimpse[step] or input.new(), input, location)
+      -- sensor generates transformation of image given input and location
+      self.glimpse[step] = sensor:updateOutput{input, location}
       
       -- rnn handles the recurrence internally
-      self.hidden[step] = self.rnn:updateOutput{location, self.glimpse[step]}
-      
-      -- main tasks are the only things that will be forwarded.
-      -- the locator is updated via the REINFORCE rule
-      self.output[step] = main:updateOutput(self.hidden[step])
+      self.output[step] = self.rnn:updateOutput{location, self.glimpse[step]}
    end
    
    return self.output
@@ -177,14 +86,13 @@ function RVA:updateGradInput(input, gradOutput)
     
    -- backward through the main and locator layers
    for step=self.nStep,1,-1 do
-      local main, locator = self:getStepModule(step)
-      local gradMain = main:updateGradInput(self.hidden[step], gradOutput[step])
+      local sensor, locator = self:getStepModule(step)
       
       if step == self.nStep then
-         self.gradHidden[step] = nn.rnn.recursiveCopy(self.gradHidden[step], gradMain)
+         self.gradHidden[step] = nn.rnn.recursiveCopy(self.gradHidden[step], gradOutput[step])
       else
-         -- gradHidden = gradMain + gradLocator
-         nn.rnn.recursiveAdd(self.gradHidden[step], gradMain)
+         -- gradHidden = gradOutput + gradLocator
+         nn.rnn.recursiveAdd(self.gradHidden[step], gradOutput[step])
       end
       
       if step == 1 then
@@ -205,9 +113,15 @@ function RVA:updateGradInput(input, gradOutput)
    -- back-propagate through time (BPTT)
    self.rnn:updateGradInputThroughTime()
    
-   -- for now, we assume self is at the input of the graph (no gradInput)
-   -- so we use a dummy gradInput pointing to input
-   self.gradInput:set(input)
+   for step=self.nStep,1,-1 do
+      local sensor = self:getStepModule(step)
+      local gradInput = sensor:updateGradInput({input, self.location[step]}, self.rnn.gradInputs[step])[1]
+      if step == self.nStep then
+         self.gradInput:resizeAs(gradInput):copy(gradInput)
+      else
+         self.gradInput:add(gradInput)
+      end
+   end
 
    return self.gradInput
 end
@@ -238,6 +152,11 @@ function RVA:accGradParameters(input, gradOutput, scale)
    end
    -- back-propagate through time (BPTT)
    self.rnn:accGradParametersThroughTime()
+   
+   for step=self.nStep,1,-1 do
+      local sensor = self:getStepModule(step)
+      sensor:accGradParameters({input, self.location[step]}, self.rnn.gradInputs[step], scale)
+   end
 end
 
 function RVA:accUpdateGradParameters(input, gradOutput, lr)
@@ -266,6 +185,11 @@ function RVA:accUpdateGradParameters(input, gradOutput, lr)
    end
    -- back-propagate through time (BPTT)
    self.rnn:accUpdateGradParametersThroughTime()
+   
+   for step=self.nStep,1,-1 do
+      local sensor = self:getStepModule(step)
+      sensor:accUpdateGradParameters({input, self.location[step]}, self.rnn.gradInputs[step], lr)
+   end
 end
 
 -- annotates image with path taken by attention
