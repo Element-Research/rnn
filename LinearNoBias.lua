654ca645c73e41f041bae5d3a05996b17ad7f8fb
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-09-05 18:30:56 -0400

    Full LinearNoBias implementation

--- LinearNoBias.lua
+++ LinearNoBias.lua
@@ -1,20 +1,65 @@
 ------------------------------------------------------------------------
 --[[ LinearNoBias ]]--
 -- Subclass of nn.Linear with no bias term
--- The implementation is maximally independent of the implementation of nn.Linear
--- (only assumption is that the bias and its gradient is stored in self.bias and self.gradBias)
--- but is slightly inefficient (the bias term is still stored in memory and its
--- gradient computed at every step).
 ------------------------------------------------------------------------
+nn = require 'nn'
 local LinearNoBias, Linear = torch.class('nn.LinearNoBias', 'nn.Linear')
 
 function LinearNoBias:__init(inputSize, outputSize)
-    Linear.__init(self, inputSize, outputSize)
-    self.bias:zero()
+   nn.Module.__init(self)
+
+   self.weight = torch.Tensor(outputSize, inputSize)
+   self.gradWeight = torch.Tensor(outputSize, inputSize)
+
+   self:reset()
+end
+
+function LinearNoBias:reset(stdv)
+   if stdv then
+      stdv = stdv * math.sqrt(3)
+   else
+      stdv = 1./math.sqrt(self.weight:size(2))
+   end
+   if nn.oldSeed then
+      for i=1,self.weight:size(1) do
+         self.weight:select(1, i):apply(function()
+            return torch.uniform(-stdv, stdv)
+         end)
+      end
+   else
+      self.weight:uniform(-stdv, stdv)
+   end
+
+   return self
+end
+
+function LinearNoBias:updateOutput(input)
+   if input:dim() == 1 then
+      self.output:resize(self.weight:size(1))
+      self.output:addmv(1, self.weight, input)
+   elseif input:dim() == 2 then
+      local nframe = input:size(1)
+      local nElement = self.output:nElement()
+      self.output:resize(nframe, self.weight:size(1))
+      if self.output:nElement() ~= nElement then
+         self.output:zero()
+      end
+      if not self.addBuffer or self.addBuffer:nElement() ~= nframe then
+         self.addBuffer = input.new(nframe):fill(1)
+      end
+      self.output:addmm(0, self.output, 1, input, self.weight:t())
+   else
+      error('input must be vector or matrix')
+   end
+
+   return self.output
 end
 
 function LinearNoBias:accGradParameters(input, gradOutput, scale)
-    Linear.accGradParameters(self, input, gradOutput, scale)
-    self.gradBias:zero()
+   scale = scale or 1
+   if input:dim() == 1 then
+      self.gradWeight:addr(scale, gradOutput, input)
+   elseif input:dim() == 2 then
+      self.gradWeight:addmm(scale, gradOutput:t(), input)
+   end
 end
-
