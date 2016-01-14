a07b7e2e024d8462b21c8797271d831146cd9221
Yang Wang <yw@discoverelement.com>
2016-01-14 15:23:49 -0500

    optimize the batch for loop

--- examples/recurrent-time-series.lua
+++ examples/recurrent-time-series.lua
@@ -2,28 +2,16 @@
 -- at https://github.com/Element-Research/rnn#rnn.Recurrent
 require 'rnn'
 
+-- experiment setup
 rho = 5 -- maximum number of time steps for BPTT
 inputSize = 6
 hiddenSize = 10
 outputSize = 6
 nIndex = 100
--- RNN
-r = nn.Recurrent(
-   hiddenSize, -- size of output
-   nn.Linear(inputSize, hiddenSize), -- input layer
-   nn.Linear(hiddenSize, hiddenSize), -- recurrent layer
-   nn.Sigmoid(), -- transfer function
-   rho
-)
 
-rnn = nn.Sequential()
-   :add(r)
-   :add(nn.Linear(hiddenSize, outputSize))
-
-criterion = nn.MSECriterion() 
-
--- dummy dataset (task is to predict next vector, given previous)
--- evaluate normal distribution function, vX is used as both input X and output Y to save memory
+-- toy dataset (task is to predict next vector, given previous)
+-- following the normal distribution 
+-- note: vX is used as both input X and output Y to save memory
 local function evalPDF(vMean, vSigma, vX)
    for i=1,vMean:size(1) do
       local b = (vX[i]-vMean[i])/vSigma[i]
@@ -54,22 +42,38 @@ for i=1,batchSize do
    table.insert(offsets, math.ceil(math.random()*batchSize))
 end
 offsets = torch.LongTensor(offsets)
---print(offsets)
+
+-- RNN
+r = nn.Recurrent(
+   hiddenSize, -- size of output
+   nn.Linear(inputSize, hiddenSize), -- input layer
+   nn.Linear(hiddenSize, hiddenSize), -- recurrent layer
+   nn.Sigmoid(), -- transfer function
+   rho
+)
+
+rnn = nn.Sequential()
+   :add(r)
+   :add(nn.Linear(hiddenSize, outputSize))
+
+criterion = nn.MSECriterion() 
 
 -- wrap rnn in to a Recursor
 rnn = nn.Recursor(rnn, rho)
 rnn:zeroGradParameters()
 -- rnn uses backwardOnline by default
 --rnn:backwardOnline()
+
+-- use Sequencer for better data handling
 rnn = nn.Sequencer(rnn)
 criterion = nn.SequencerCriterion(criterion)
 print(rnn)
 
-updateInterval = 4 -- note that updateInterval < rho
+-- train rnn model
 lr = 0.001 -- learning rate
-minErr = outputSize
+nIterations = 1000 -- max loop number
+minErr = outputSize -- report min error
 minK = 0
-nIterations = 1000
 avgErrs = torch.Tensor(nIterations):fill(0)
 for k = 1, nIterations do --while true do
    -- 1. create a sequence of rho time-steps
@@ -79,11 +83,7 @@ for k = 1, nIterations do --while true do
       inputs[step] = sequence:index(1, offsets)
       -- batch of targets
       offsets:add(1) -- increase indices by 1
-      for i=1,batchSize do
-         if offsets[i] > nIndex then
-            offsets[i] = 1
-         end
-      end
+      offsets[offsets:gt(nIndex)] = 1
       targets[step] = sequence:index(1, offsets)
    end
 
@@ -94,8 +94,13 @@ for k = 1, nIterations do --while true do
 
    -- report errors
    local err = criterion:forward(outputs, targets)
-   print('Iter: ' .. k .. ' Err: ' .. err)
+   print('Iter: ' .. k .. '   Err: ' .. err)
    --print(' Input:  ', inputs); print(' Output: ', outputs); print(' Target: ', targets)
+   avgErrs[k] = err
+   if avgErrs[k] < minErr then
+      minErr = avgErrs[k]
+      minK = k
+   end
 
    -- 3. backward sequence through rnn (i.e. backprop through time)
    local gradOutputs = criterion:backward(outputs, targets)
@@ -103,12 +108,6 @@ for k = 1, nIterations do --while true do
 
    -- 4. updates parameters
    rnn:updateParameters(lr)
-
-   avgErrs[k] = err
-   if avgErrs[k] < minErr then
-      minErr = avgErrs[k]
-      minK = k
-   end
 end -- nIterations
 
 --print(avgErrs)
