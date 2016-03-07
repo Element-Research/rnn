ae1ead45026541e388747e737391af23968ca6d0
nicholas-leonard <nick@nikopia.org>
2016-03-07 16:13:08 -0500

    initial commit for rnnlm w/o dp

--- examples/recurrent-language-model.lua
+++ examples/recurrent-language-model.lua
@@ -1,118 +1,129 @@
-require 'dp'
+require 'paths'
 require 'rnn'
+local dl = require 'dataload'
 
-version = 1
+version = 2
 
---[[command line arguments]]--
+--[[ command line arguments ]]--
 cmd = torch.CmdLine()
 cmd:text()
 cmd:text('Train a Language Model on PennTreeBank dataset using RNN or LSTM or GRU')
 cmd:text('Example:')
-cmd:text("recurrent-language-model.lua --cuda --useDevice 2 --progress --zeroFirst --cutoffNorm 4 --rho 10")
+cmd:text('recurrent-language-model.lua --cuda --device 2 --progress --cutoff 4 --seqlen 10')
 cmd:text('Options:')
-cmd:option('--learningRate', 0.05, 'learning rate at t=0')
-cmd:option('--minLR', 0.00001, 'minimum learning rate')
-cmd:option('--saturateEpoch', 400, 'epoch at which linear decayed LR will reach minLR')
+cmd:option('--lr', 0.05, 'learning rate at t=0')
+cmd:option('--minlr', 0.00001, 'minimum learning rate')
+cmd:option('--saturate', 400, 'epoch at which linear decayed LR will reach minlr')
 cmd:option('--momentum', 0.9, 'momentum')
-cmd:option('--maxOutNorm', -1, 'max l2-norm of each layer\'s output neuron weights')
-cmd:option('--cutoffNorm', -1, 'max l2-norm of concatenation of all gradParam tensors')
-cmd:option('--batchSize', 32, 'number of examples per batch')
+cmd:option('--maxoutnorm', -1, 'max l2-norm of each layer\'s output neuron weights')
+cmd:option('--cutoff', -1, 'max l2-norm of concatenation of all gradParam tensors')
+cmd:option('--batchsize', 32, 'number of examples per batch')
 cmd:option('--cuda', false, 'use CUDA')
-cmd:option('--useDevice', 1, 'sets the device (GPU) to use')
-cmd:option('--maxEpoch', 1000, 'maximum number of epochs to run')
-cmd:option('--maxTries', 50, 'maximum number of epochs to try to find a better local minima for early-stopping')
+cmd:option('--device', 1, 'sets the device (GPU) to use')
+cmd:option('--maxepoch', 1000, 'maximum number of epochs to run')
+cmd:option('--earlystop', 50, 'maximum number of epochs to wait to find a better local minima for early-stopping')
 cmd:option('--progress', false, 'print progress bar')
 cmd:option('--silent', false, 'don\'t print anything to stdout')
 cmd:option('--uniform', 0.1, 'initialize parameters using uniform distribution between -uniform and uniform. -1 means default initialization')
+cmd:option('--nce', false, 'train with Noise Contrastive Estimation')
+cmd:option('--k', 25, 'how many noise samples to use for NCE')
 
 -- recurrent layer 
 cmd:option('--lstm', false, 'use Long Short Term Memory (nn.LSTM instead of nn.Recurrent)')
 cmd:option('--gru', false, 'use Gated Recurrent Units (nn.GRU instead of nn.Recurrent)')
-cmd:option('--rho', 5, 'back-propagate through time (BPTT) for rho time-steps')
-cmd:option('--hiddenSize', '{200}', 'number of hidden units used at output of each recurrent layer. When more than one is specified, RNN/LSTMs/GRUs are stacked')
-cmd:option('--zeroFirst', false, 'first step will forward zero through recurrence (i.e. add bias of recurrence). As opposed to learning bias specifically for first step.')
-cmd:option('--dropout', false, 'apply dropout after each recurrent layer')
-cmd:option('--dropoutProb', 0.5, 'probability of zeroing a neuron (dropout probability)')
+cmd:option('--seqlen', 5, 'sequence length : back-propagate through time (BPTT) for this many time-steps')
+cmd:option('--hiddensize', '{200}', 'number of hidden units used at output of each recurrent layer. When more than one is specified, RNN/LSTMs/GRUs are stacked')
+cmd:option('--dropout', 0, 'apply dropout with this probability after each rnn layer. dropout <= 0 disables it.')
 
 -- data
-cmd:option('--trainEpochSize', -1, 'number of train examples seen between each epoch')
-cmd:option('--validEpochSize', -1, 'number of valid examples used for early stopping and cross-validation') 
+cmd:option('--trainsize', -1, 'number of train examples seen between each epoch')
+cmd:option('--validsize', -1, 'number of valid examples used for early stopping and cross-validation') 
+cmd:option('--savepath', paths.concat(dl.SAVE_PATH, 'rnnlm'), 'path to directory where experiment log (includes model) will be saved')
+cmd:option('--id', '', 'id string of this experiment (used to name output file) (defaults to a unique id)')
 
 cmd:text()
-opt = cmd:parse(arg or {})
-opt.hiddenSize = dp.returnString(opt.hiddenSize)
+local opt = cmd:parse(arg or {})
+opt.hiddensize = loadstring(" return "..opt.hiddensize)()
 if not opt.silent then
    table.print(opt)
 end
+opt.id = opt.id == '' and ('ptb' .. ':' .. dl.uniqueid()) or opt.id
 
---[[Data]]--
+--[[ data set ]]--
 
-ds = dp.PennTreeBank{
-   context_size=opt.rho, 
-   recurrent=true
-}
-ds:validSet():contextSize(opt.batchSize)
-ds:testSet():contextSize(opt.batchSize)
-
---[[Model]]--
-
--- language model
-lm = nn.Sequential()
-
-local inputSize = opt.hiddenSize[1]
-for i,hiddenSize in ipairs(opt.hiddenSize) do 
-
-   if i~= 1 and (not opt.lstm) and (not opt.gru) then
-      lm:add(nn.Sequencer(nn.Linear(inputSize, hiddenSize)))
-   end
-   
-   -- recurrent layer
-   local rnn
-   if opt.gru then
-      -- Gated Recurrent Units
-      rnn = nn.Sequencer(nn.GRU(inputSize, hiddenSize))
-   elseif opt.lstm then
-      -- Long Short Term Memory
-      rnn = nn.Sequencer(nn.FastLSTM(inputSize, hiddenSize))
-   else
-      -- simple recurrent neural network
-      rnn = nn.Recurrent(
-         hiddenSize, -- first step will use nn.Add
-         nn.Identity(), -- for efficiency (see above input layer) 
-         nn.Linear(hiddenSize, hiddenSize), -- feedback layer (recurrence)
-         nn.Sigmoid(), -- transfer function 
-         99999 -- maximum number of time-steps per sequence
-      )
-      if opt.zeroFirst then
-         -- this is equivalent to forwarding a zero vector through the feedback layer
-         rnn.startModule:share(rnn.feedbackModule, 'bias')
-      end
-      rnn = nn.Sequencer(rnn)
-   end
-
-   lm:add(rnn)
-   
-   if opt.dropout then -- dropout it applied between recurrent layers
-      lm:add(nn.Sequencer(nn.Dropout(opt.dropoutProb)))
-   end
-   
-   inputSize = hiddenSize
+local trainset, validset, testset = dl.loadPTB({opt.batchsize,1,1})
+if not opt.silent then 
+   print("Vocabulary size : "..#trainset.ivocab) 
+   print("Train set split into "..opt.batchsize.." sequences of length "..trainset:size())
 end
+
+--[[ language model ]]--
+
+local lm = nn.Sequential()
 
 -- input layer (i.e. word embedding space)
-lm:insert(nn.SplitTable(1,2), 1) -- tensor to table of tensors
+local lookup = nn.LookupTable(#trainset.ivocab, opt.hiddensize[1])
+lookup.maxoutnorm = -1 -- prevent weird maxoutnorm behaviour
+lm:add(lookup) -- input is seqlen x batchsize
+lm:add(nn.SplitTable(1)) -- tensor to table of tensors
 
-if opt.dropout then
-   lm:insert(nn.Dropout(opt.dropoutProb), 1)
+if opt.dropout > 0 then
+   lm:insert(nn.Dropout(opt.dropout), 1)
 end
 
-lookup = nn.LookupTable(ds:vocabularySize(), opt.hiddenSize[1])
-lookup.maxOutNorm = -1 -- disable maxParamNorm on the lookup table
-lm:insert(lookup, 1)
+-- rnn layers
+local stepmodule = nn.Sequential() -- applied at each time-step
+local inputsize = opt.hiddensize[1]
+for i,hiddensize in ipairs(opt.hiddensize) do 
+   local rnn
+   
+   if opt.gru then -- Gated Recurrent Units
+      rnn = nn.GRU(inputsize, hiddensize)
+   elseif opt.lstm then -- Long Short Term Memory
+      require 'nngraph'
+      nn.FastLSTM.usenngraph = true -- faster
+      rnn = nn.FastLSTM(inputsize, hiddensize)
+   else -- simple recurrent neural network
+      local rm =  nn.Sequential() -- input is {x[t], h[t-1]}
+         :add(nn.ParallelTable()
+            :add(i==1 and nn.Identity() or nn.Linear(inputsize, hiddensize)) -- input layer
+            :add(nn.Linear(hiddensize, hiddensize))) -- recurrent layer
+         :add(nn.CAddTable()) -- merge
+         :add(nn.Sigmoid()) -- transfer
+      rnn = nn.Recurrence(rm, hiddensize, 1)
+   end
+
+   stepmodule:add(rnn)
+   
+   if opt.dropout > 0 then
+      stepmodule:add(nn.Dropout(opt.dropout))
+   end
+   
+   inputsize = hiddensize
+end
 
 -- output layer
-lm:add(nn.Sequencer(nn.Linear(inputSize, ds:vocabularySize())))
-lm:add(nn.Sequencer(nn.LogSoftMax()))
+if opt.nce then
+   local unigram = torch.FloatTensor(#trainset.ivocab)
+   for word,freq in pairs(trainset.wordfreq) do
+      unigram[trainset.vocab[word]] = freq
+   end
+   stepmodule:add(nn.NCEModule(inputsize, #trainset.ivocab, opt.k, unigram))
+else
+   stepmodule:add(nn.Linear(inputsize, #trainset.ivocab))
+   stepmodule:add(nn.LogSoftMax())
+end
+
+-- encapsulate stepmodule into a Sequencer
+lm:add(nn.Sequencer(stepmodule))
+
+-- remember previous state between batches
+lm:remember((opt.lstm or opt.gru) and 'both' or 'eval')
+
+if not opt.silent then
+   print"Language Model:"
+   print(lm)
+end
 
 if opt.uniform > 0 then
    for k,param in ipairs(lm:parameters()) do
@@ -120,88 +131,146 @@ if opt.uniform > 0 then
    end
 end
 
--- will recurse a single continuous sequence
-lm:remember((opt.lstm or opt.gru) and 'both' or 'eval')
+--[[ loss function ]]--
 
---[[Propagators]]--
+local crit = opt.nce and nn.NCECriterion() or nn.ClassNLLCriterion()
 
--- linear decay
-opt.decayFactor = (opt.minLR - opt.learningRate)/opt.saturateEpoch
+-- target is also seqlen x batchsize.
+local targetmodule = nn.SplitTable(1)
+if opt.cuda then
+   targetmodule = nn.Sequential()
+      :add(nn.Convert())
+      :add(targetmodule)
+end
+ 
+local criterion = nn.ModuleCriterion(nn.SequencerCriterion(crit), nil, targetmodule)
 
-train = dp.Optimizer{
-   loss = nn.ModuleCriterion(
-            nn.SequencerCriterion(nn.ClassNLLCriterion()), 
-            nn.Identity(), 
-            opt.cuda and nn.Sequencer(nn.Convert()) or nn.Identity()
-         ),
-   epoch_callback = function(model, report) -- called every epoch
-      if report.epoch > 0 then
-         opt.learningRate = opt.learningRate + opt.decayFactor
-         opt.learningRate = math.max(opt.minLR, opt.learningRate)
-         if not opt.silent then
-            print("learningRate", opt.learningRate)
-            if opt.meanNorm then
-               print("mean gradParam norm", opt.meanNorm)
-            end
-         end
-      end
-   end,
-   callback = function(model, report) -- called every batch
-      if opt.cutoffNorm > 0 then
-         local norm = model:gradParamClip(opt.cutoffNorm) -- affects gradParams
-         opt.meanNorm = opt.meanNorm and (opt.meanNorm*0.9 + norm*0.1) or norm
-      end
-      model:updateGradParameters(opt.momentum) -- affects gradParams
-      model:updateParameters(opt.learningRate) -- affects params
-      model:maxParamNorm(opt.maxOutNorm) -- affects params
-      model:zeroGradParameters() -- affects gradParams 
-   end,
-   feedback = dp.Perplexity(),  
-   sampler = dp.TextSampler{epoch_size = opt.trainEpochSize, batch_size = opt.batchSize}, 
-   progress = opt.progress
-}
-valid = dp.Evaluator{
-   feedback = dp.Perplexity(),  
-   sampler = dp.TextSampler{epoch_size = opt.validEpochSize, batch_size = 1},
-   progress = opt.progress
-}
-tester = dp.Evaluator{
-   feedback = dp.Perplexity(),  
-   sampler = dp.TextSampler{batch_size = 1} 
-}
-
---[[Experiment]]--
-
-xp = dp.Experiment{
-   model = lm,
-   optimizer = train,
-   validator = valid,
-   tester = tester,
-   observer = {
-      dp.FileLogger(),
-      dp.EarlyStopper{
-         max_epochs = opt.maxTries, 
-         error_report={'validator','feedback','perplexity','ppl'}
-      }
-   },
-   random_seed = os.time(),
-   max_epoch = opt.maxEpoch,
-   target_module = nn.SplitTable(1,1):type('torch.IntTensor')
-}
-
---[[GPU or CPU]]--
+--[[ CUDA ]]--
 
 if opt.cuda then
-   require 'cutorch'
    require 'cunn'
-   cutorch.setDevice(opt.useDevice)
-   xp:cuda()
+   cutorch.setDevice(opt.device)
+   lm:cuda()
+   criterion:cuda()
 end
 
-xp:verbose(not opt.silent)
-if not opt.silent then
-   print"Language Model :"
-   print(lm)
+--[[ experiment log ]]--
+
+-- is saved to file every time a new validation minima is found
+local xplog = {}
+xplog.opt = opt -- save all hyper-parameters and such
+xplog.dataset = 'PennTreeBank'
+-- will only serialize params
+xplog.model = nn.Serial(net)
+xplog.model:mediumSerial()
+xplog.criterion = criterion
+-- keep a log of NLL for each epoch
+xplog.trainppl = {}
+xplog.valppl = {}
+-- will be used for early-stopping
+xplog.minvalppl = 99999999
+xplog.epoch = 0
+local ntrial = 0
+paths.mkdir(opt.savepath)
+
+-- linear decay
+opt.decayFactor = (opt.minlr - opt.lr)/opt.saturate
+
+local epoch = 1
+opt.trainsize = opt.trainsize == -1 and trainset:size() or opt.trainsize
+opt.validsize = opt.validsize == -1 and validset:size() or opt.validsize
+while opt.maxepoch <= 0 or epoch <= opt.maxepoch do
+   print("")
+   print("Epoch #"..epoch.." :")
+
+   -- 1. training
+   
+   local a = torch.Timer()
+   lm:training()
+   local sumErr = 0
+   for i, inputs, targets in trainset:subiter(opt.seqlen, opt.trainsize) do
+   
+      -- forward
+      local outputs = lm:forward(inputs)
+      local err = criterion:forward(outputs, targets)
+      sumErr = sumErr + err
+      
+      -- backward 
+      local gradOutputs = criterion:backward(outputs, targets)
+      lm:zeroGradParameters()
+      lm:backward(inputs, gradOutputs)
+      
+      -- update
+      if opt.cutoff > 0 then
+         local norm = lm:gradParamClip(opt.cutoff) -- affects gradParams
+         opt.meanNorm = opt.meanNorm and (opt.meanNorm*0.9 + norm*0.1) or norm
+      end
+      lm:updateGradParameters(opt.momentum) -- affects gradParams
+      lm:updateParameters(opt.lr) -- affects params
+      lm:maxParamNorm(opt.maxoutnorm) -- affects params
+
+      if opt.progress then
+         xlua.progress(math.min(i + opt.seqlen, opt.trainsize), opt.trainsize)
+      end
+
+      if i % 1000 == 0 then
+         collectgarbage()
+      end
+
+   end
+   
+   -- learning rate decay
+   opt.lr = math.max(opt.minlr, opt.lr + opt.decayFactor)
+   if not opt.silent then
+      print("learning rate", opt.lr)
+      if opt.meanNorm then
+         print("mean gradParam norm", opt.meanNorm)
+      end
+   end
+
+   if cutorch then cutorch.synchronize() end
+   local speed = a:time().real/opt.trainsize
+   print(string.format("Speed : %f time/batch ", speed))
+
+   local ppl = torch.exp(sumErr/opt.trainsize)
+   print("Training PPL : "..ppl)
+
+   xplog.trainppl[epoch] = ppl
+
+   -- 2. cross-validation
+
+   lm:evaluate()
+   local sumErr = 0
+   for i, inputs, targets in validset:subiter(opt.seqlen, opt.validsize) do
+   
+      local outputs = lm:forward(inputs)
+      local err = criterion:forward(outputs, targets)
+      sumErr = sumErr + err
+   end
+
+   local ppl = torch.exp(sumErr/opt.validsize)
+   print("Validation PPL : "..ppl)
+
+   xplog.valppl[epoch] = ppl
+   ntrial = ntrial + 1
+
+   -- early-stopping
+   if ppl < xplog.minvalppl then
+      -- save best version of model
+      xplog.minvalppl = ppl
+      xplog.epoch = epoch 
+      local filename = paths.concat(opt.savepath, opt.id..'.t7')
+      print("Found new minima. Saving to "..filename)
+      torch.save(filename, xplog)
+      ntrial = 0
+   elseif ntrial >= opt.earlystop then
+      print("No new minima found after "..ntrial.." epochs.")
+      print("Stopping experiment.")
+      print("Best model can be found in "..paths.concat(opt.savepath, opt.id..'.t7'))
+      os.exit()
+   end
+
+   collectgarbage()
+   epoch = epoch + 1
 end
 
-xp:run(ds)
